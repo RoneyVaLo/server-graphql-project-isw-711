c3dff852a08a9d4108eca42116e60ffc8e8695f2
Roney Valdelomar <rvaldelomarlopez@gmail.com>
2023-08-18 20:32:01 -0600

    create models and controllers needed

---
+++ controllers/promptController.js
@@ -0,0 +1,40 @@
+const promptModel = require('../models/promptModel');
+
+
+const promptGetAll = (req, res) => {
+    return promptModel.find()
+        .then(prompts => {
+            return prompts;
+        })
+        .catch(error => {
+            console.log('There was an error:', error);
+            return error;
+        });
+};
+
+const promptSearch = (req, res) => {
+    const { name, tag } = req
+    let filters = {};
+
+    // If the user has entered a search term in either field then we will filter by that value and send back all matching results to the client side
+    if (name) {
+        filters.name = { $regex: `${name}`, $options: 'i' };
+    };
+    if (tag) {
+        filters.tags = { $regex: `${tag}`, $options: 'i' };
+    };
+
+    return promptModel.find(filters)
+        .then(prompts => {
+            return prompts;
+        })
+        .catch(error => {
+            console.log('There was an error:', error);
+            return error;
+        });
+};
+
+module.exports = {
+    promptGetAll,
+    promptSearch
+};
