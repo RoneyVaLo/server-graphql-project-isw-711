d146fd3e90de105f88c7ddc627fb6578fd238b6a
Roney Valdelomar <rvaldelomarlopez@gmail.com>
2023-08-18 20:45:30 -0600

    update schema structure

--- graphql-schema.js
+++ graphql-schema.js
@@ -1,5 +1,9 @@
 const { buildSchema } = require('graphql');
+
+//* scalar JSON: Used to create custom type to represent JSON structures
 exports.graphQLschema = buildSchema(`
+    scalar JSON
+
     type Query {
         getAllPrompts: [Prompt]
         searchPrompts(name: String, tag: String): [Prompt]
@@ -7,8 +11,11 @@ exports.graphQLschema = buildSchema(`
 
     type Prompt {
         _id: ID!
+        user: String
         name: String
         type: String
+        data: JSON
         tags: [String]
+        results: [JSON]
     }
 `);
